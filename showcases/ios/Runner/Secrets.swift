c8261a8aceaf20f831c07e3262c9f43a78502d1f
Intergalactic Igor <malte.baumann+bot@img.ly>
2024-11-27 17:38:46 +0100

    Update examples for v1.40.0-rc.2

--- showcases/ios/Runner/Secrets.swift
+++ showcases/ios/Runner/Secrets.swift
@@ -2,8 +2,8 @@ class Secrets {
   static var unsplashHost: String {
     if let defines = Bundle.main.infoDictionary?["DART_DEFINES"] as? String,
        let unsplashHost = defines.components(separatedBy: ",").first,
-       let unsplashHostData = Data(base64Encoded: unsplashHost) {
-      let decoded = String(decoding: unsplashHostData, as: UTF8.self)
+       let unsplashHostData = Data(base64Encoded: unsplashHost),
+       let decoded = String(data: unsplashHostData, encoding: .utf8) {
       let decodedComponents = decoded.components(separatedBy: "=")
       guard decodedComponents.count > 1, let key = decodedComponents.first,
             key == "CESDK_UNSPLASH_HOST" else { return "" }
