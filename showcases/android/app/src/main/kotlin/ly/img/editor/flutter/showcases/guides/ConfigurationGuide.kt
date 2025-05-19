aa62bd9729e48216d325a374b531360a0740ba36
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-05-19 09:00:04 +0200

    Update examples for v1.51.0-rc.5

--- showcases/android/app/src/main/kotlin/ly/img/editor/flutter/showcases/guides/ConfigurationGuide.kt
+++ showcases/android/app/src/main/kotlin/ly/img/editor/flutter/showcases/guides/ConfigurationGuide.kt
@@ -1,15 +1,21 @@
 package ly.img.editor.flutter.showcases.guides
 
-// #highlight-import
+// highlight-import
 import androidx.compose.runtime.Composable
+import ly.img.camera.core.CameraLayoutMode
+import ly.img.camera.core.CameraMode
+import ly.img.camera.core.CameraResult
+import ly.img.camera.core.CaptureVideo
+import ly.img.camera.flutter.plugin.IMGLYCameraPlugin
 import ly.img.editor.flutter.plugin.IMGLYEditorPlugin
 import ly.img.editor.flutter.plugin.builder.EditorBuilder
 import ly.img.editor.flutter.plugin.builder.EditorBuilderResult
 import ly.img.editor.flutter.plugin.model.EditorSettings
-// #highlight-import
+
+// highlight-import
 
 private fun useCustomEditor() {
-    // #highlight-closure
+    // highlight-closure
     IMGLYEditorPlugin.builderClosure = { _, metadata ->
         if (metadata?.get("custom") == true) {
             EditorBuilder.custom { settings, _, _, result, onClose ->
@@ -21,7 +27,71 @@ private fun useCustomEditor() {
             EditorBuilder.design()
         }
     }
-    // #highlight-closure
+    // highlight-closure
+}
+
+private fun customizeCamera() {
+    // highlight-camera-configuration
+    // highlight-camera-configuration-closure
+    // Configure the [CaptureVideo.Input].
+    IMGLYCameraPlugin.configurationClosure = {
+        /*
+         * This closure requires returning a [CaptureVideo.Input].
+         *
+         * You can access the following parameters:
+         * - "cameraSettings": The CameraSettings passed from the Flutter side.
+         * - "engineConfiguration": The EngineConfiguration that includes your license.
+         * - "metadata": Metadata passed from the Flutter side.
+         * - "videoUri": The video reaction URL passed from Flutter, or null.
+         */
+
+        if (metadata["is_reactions"] == true) {
+            CaptureVideo.Input(
+                engineConfiguration = engineConfiguration,
+                cameraMode = CameraMode.Reaction(
+                    videoUri ?: throw RuntimeException("Missing video URL."),
+                    cameraLayoutMode = CameraLayoutMode.Horizontal,
+                    positionsSwapped = false,
+                ),
+            )
+        } else {
+            // Simply return null to use the default camera configuration.
+            null
+        }
+    }
+
+    // highlight-camera-configuration-closure
+    // highlight-camera-result-closure
+    // Adding custom metadata to the CameraResult.
+    IMGLYCameraPlugin.resultClosure = {
+        /*
+         * This closure allows you to add custom metadata of type [Map<String, Any?>] to the CameraResult.
+         * You can access the following values:
+         * - "cameraResult": The native CameraResult returned by the camera.
+         * - "inputMetadata": Metadata passed from the Flutter side when the camera was opened.
+         *
+         * The return value must be a [Map<String, Any?>], which will be passed to the Dart-side
+         * representation of the CameraResult.
+         *
+         * Notes:
+         * - Use `mapOf<String, Any?>()` to return key-value pairs.
+         * - Return `null` if no metadata should be added.
+         */
+
+        // Example: Read the result data...
+        val recordings = when (val result = cameraResult) {
+            is CameraResult.Record -> result.recordings
+            is CameraResult.Reaction -> result.reaction
+            else -> emptyList()
+        }
+
+        // Return a custom metadata map
+        mapOf(
+            "MY_CUSTOM_KEY" to "MY_CUSTOM_VALUE",
+        )
+    }
+    // highlight-camera-result-closure
+    // highlight-camera-configuration
 }
 
 @Composable
@@ -29,4 +99,5 @@ private fun CustomEditor(
     settings: EditorSettings,
     result: EditorBuilderResult,
     onClose: (Throwable?) -> Unit,
-) {}
+) {
+}
--- showcases/ios/Runner/Guides/ConfigurationGuide.swift
+++ showcases/ios/Runner/Guides/ConfigurationGuide.swift
@@ -1,11 +1,12 @@
-// #highlight-import
+// highlight-import
+import imgly_camera
 import imgly_editor
 import SwiftUI
 
-// #highlight-import
+// highlight-import
 
 func useCustomEditor() {
-  // #highlight-closure
+  // highlight-closure
   IMGLYEditorPlugin.builderClosure = { _, metadata in
     // Make decisions based on your own metadata.
     if metadata?["use_custom_editor"] as? Bool == true {
@@ -18,7 +19,26 @@ func useCustomEditor() {
       EditorBuilder.design()
     }
   }
-  // #highlight-closure
+  // highlight-closure
+}
+
+func configureCamera() {
+  // highlight-camera-configuration
+  // highlight-camera-configuration-closure
+  IMGLYCameraPlugin.builderClosure = { metadata in
+    // Make decisions based on your own metadata.
+    if metadata?["use_custom_camera"] as? Bool == true {
+      // Return your custom camera.
+      CameraBuilder.custom { settings, url, metadata, result in
+        CustomCamera(settings: settings, url: url, metadata: metadata, result: result)
+      }
+    } else {
+      // Return a custom or prebuilt camera.
+      CameraBuilder.default()
+    }
+  }
+  // highlight-camera-configuration-closure
+  // highlight-camera-configuration
 }
 
 private struct CustomEditor: View {
@@ -26,5 +46,18 @@ private struct CustomEditor: View {
 
   var body: some View {
     Text("Custom Editor")
+  }
+}
+
+private struct CustomCamera: View {
+  init(
+    settings _: CameraSettings,
+    url _: URL?,
+    metadata _: [String: Any]?,
+    result _: @escaping CameraBuilder.CameraBuilderResult
+  ) {}
+
+  var body: some View {
+    Text("Custom Camera")
   }
 }
