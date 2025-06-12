6fc60193713e104fc7d931ca5788071064e8c828
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-06-12 18:04:17 +0200

    Update examples for v1.53.0

--- showcases/guides/camera-guides-configuration/camera_flutter.dart
+++ showcases/guides/camera-guides-configuration/camera_flutter.dart
@@ -10,12 +10,12 @@ class CameraFlutter {
         CameraSettings(license: "YOUR_LICENSE", userId: "YOUR_USER_ID");
     // highlight-configuration
 
-    // highlight-camera
+    // highlight-camera-call
     final _ = await IMGLYCamera.openCamera(
       settings,
       // Optional, if you want to react to a video
       video: 'https://sample-videos.com/video321/mp4/720/big_buck_bunny_720p_2mb.mp4',
     )
-    // highlight-camera
+    // highlight-camera-call
   }
 }
